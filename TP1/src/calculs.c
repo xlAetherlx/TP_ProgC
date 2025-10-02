3555e206fdf7e710d5b033a3b455aa0f19340291
Valdow <orianne.val.lopez@gmail.com>
2025-10-02 11:40:55 +0200

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 20;
+    int num2 = 5;
+    char op = '+';
+    
+    switch (op) {
+        case '+':
+            printf("%d + %d = %d\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%d - %d = %d\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%d * %d = %d\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            printf("%d / %d = %d\n", num1, num2, num1 / num2);
+            break;
+        case '%':
+            printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+            break;
+        case '&':
+            printf("%d & %d = %d\n", num1, num2, num1 & num2);
+            break;
+        case '|':
+            printf("%d | %d = %d\n", num1, num2, num1 | num2);
+            break;
+        case '~':
+            printf("~%d = %d\n", num1, ~num1);
+            break;
+        default:
+            printf("Operateur non reconnu\n");
+            break;
+    }
+    
+    return 0;
+}
 
