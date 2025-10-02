991b5f2032594e2ed1b44744f97db3e967c8be93
Valdow <orianne.val.lopez@gmail.com>
2025-10-02 12:24:55 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,43 +1,21 @@
-/*#include <stdio.h>
-
-int main() {
-    int compteur = 5;
-    
-    for (int i = 1; i <= compteur; i++) {
-        for (int j = 1; j <= i; j++) {
-            if (j == 1 || j == i || i == compteur) {
-                printf("* ");
-            } else {
-                printf("# ");
-            }
-        }
-        printf("\n");
-    }
-    
-    return 0;
-}*/
-
 #include <stdio.h>
-
 int main() {
     int compteur = 5;
     int i = 1;
-    
+
     while (i <= compteur) {
-        int j = compteur;
-        while (j >= i) {
+        int j = 1;
+        while (j <= i) {
             if (j == 1 || j == i || i == compteur) {
                 printf("* ");
             } else {
                 printf("# ");
             }
-            j--;
+            j++;
         }
         printf("\n");
         i++;
     }
-    
+
     return 0;
 }
-
-
