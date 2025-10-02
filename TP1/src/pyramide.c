84a141119e9256b0ce73240f8256724594d855d8
Valdow <orianne.val.lopez@gmail.com>
2025-10-02 11:57:30 +0200

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int n = 5;
+    int n = 5; 
     int i, j;
     
     printf("Génération de la pyramide de nombres...\n\n");
     
     for (i = 1; i <= n; i++) {
-      
+        
         for (j = 1; j <= n - i; j++) {
-            printf("  "); 
+            printf(" ");
         }
         
         for (j = 1; j <= i; j++) {
@@ -19,7 +19,7 @@ int main() {
         for (j = i - 1; j >= 1; j--) {
             printf("%d", j);
         }
-      
+    
         printf("\n");
     }
     
