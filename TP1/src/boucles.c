1f67f7193b60d65fe55f54121c51bbba6d74cf4d
Valdow <orianne.val.lopez@gmail.com>
2025-10-02 11:13:52 +0200

    Update boucles.c

---
+++ TP1/src/boucles.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int compteur = 5;
+    
+    for (int i = 1; i <= compteur; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
