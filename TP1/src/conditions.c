165379af34ea91273d6009c64402dd20c269598c
Valdow <orianne.val.lopez@gmail.com>
2025-10-02 11:38:10 +0200

    Update conditions.c

---
+++ TP1/src/conditions.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int somme = 0;
+    
+    for (int i = 1; i <= 1000; i++) {
+        if (i % 11 == 0) {
+            continue;
+        }
+        
+        if (i % 5 == 0 || i % 7 == 0) {
+            somme += i;
+        }
+        
+        if (somme > 5000) {
+            break;
+        }
+    }
+    
+    printf("Somme finale = %d\n", somme);
+    
+    return 0;
+}
