19a41e6e03169e12ae32ba30fedff85de6f1451d
Valdow <orianne.val.lopez@gmail.com>
2025-10-03 11:41:36 +0200

    Update bits.c

---
+++ TP2/src/bits.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int d = 0x00880000;
+    int resultat = 0;
+    int taille = sizeof(int) * 8;
+    int pos_bit4 = taille - 4;
+    int pos_bit20 = taille - 20;
+    int bit4 = (d >> pos_bit4) & 1;
+    int bit20 = (d >> pos_bit20) & 1;
+    if (bit4 == 1 && bit20 == 1) {
+        resultat = 1;
+    } else {
+        resultat = 0;
+    }
+    printf("%d\n", resultat);
+    return 0;
+}
